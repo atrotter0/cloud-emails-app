84e8776c0a519d7af8924cda0165910235ac5fb1
Abel Trotter <abel.trotter@getg5.com>
2017-04-19 16:46:03 -0700

    Add regex for user input

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,12 +10,11 @@ class HomeController < ApplicationController
 
   def check_params
     email_cls_urn = params[:email_cls_urn]
-    override_cls_urn = params[:override_cls_urn] 
-    # validate and run export depending on params passed
-    if email_cls_urn.blank? != true
-      run_emails_export email_cls_urn
-    elsif override_cls_urn.blank? != true
-      run_overrides_export override_cls_urn
+    override_cls_urn = params[:override_cls_urn]
+    if email_cls_urn =~ /^g5-.*$/
+      run_emails_export(email_cls_urn)
+    elsif override_cls_urn =~ /^g5-.*$/
+      run_overrides_export(override_cls_urn)
     else
       trigger_redirect
     end
